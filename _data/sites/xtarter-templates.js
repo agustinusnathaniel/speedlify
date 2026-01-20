f937b96ff176d2aded2b6adc51ff577a63913df9
Agustinus Nathaniel <berserk.nate@gmail.com>
2026-01-20 14:45:29 +0700

    Remove unused URLs from xtarter-templates.js
    Removed several URLs from the xtarter templates list.

--- _data/sites/xtarter-templates.js
+++ _data/sites/xtarter-templates.js
@@ -15,10 +15,6 @@ module.exports = {
   urls: [
     "https://nextarter-chakra.sznm.dev/",
     "https://nextarter-tailwind.sznm.dev",
-    "https://vite-react-chakra-starter.sznm.dev",
-    "https://vite-react-tailwind-starter.sznm.dev",
-    "https://vite-antd-dashboard.sznm.dev",
-    "https://vite-mantine-dashboard.sznm.dev",
-    "https://rsbuild-react-chakra-starter.sznm.dev"
+    "https://vite-react-tailwind-starter.sznm.dev"
   ],
 };
